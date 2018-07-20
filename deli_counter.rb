88a8e8b6050c61b067608eafa70e7f7423c9be99
Peggy Lee <peggyleesix@gmail.com>
2018-07-20 19:19:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+  puts "The line is currently empty."
+else
+  line_updating = "The line is currently:"
+  katz_deli.each.with_index(1) do |person, index|
+    line_updating << " #{index}. #{person}"
+  end
+  puts line_updating
+end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli << name
+  puts "Welcome, #{name}, you are number#{katz_deli.length} in line."
+end
